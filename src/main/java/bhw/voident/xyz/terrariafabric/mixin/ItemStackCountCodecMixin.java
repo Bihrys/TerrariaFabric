5a5b8548e5f594756696941625b4ac493d8bf482
Eric Bai <3200769991@qq.com>
2026-03-13 22:14:14 +0800

    修bug

---
+++ src/main/java/bhw/voident/xyz/terrariafabric/mixin/ItemStackCountCodecMixin.java
@@ -0,0 +1,15 @@
+package bhw.voident.xyz.terrariafabric.mixin;
+
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ItemStack.class)
+public class ItemStackCountCodecMixin {
+
+    @ModifyConstant(method = "method_57371", constant = @Constant(intValue = 99))
+    private static int terrariafabric$raiseCountCodecLimit(int original) {
+        return 9999;
+    }
+}
