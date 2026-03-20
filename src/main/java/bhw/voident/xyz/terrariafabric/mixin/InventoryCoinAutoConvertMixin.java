a76bb392e90907b0a4d5e243b593ccea063741d3
Eric Bai <3200769991@qq.com>
2026-03-20 23:41:00 +0800

    update

---
+++ src/main/java/bhw/voident/xyz/terrariafabric/mixin/InventoryCoinAutoConvertMixin.java
@@ -0,0 +1,28 @@
+package bhw.voident.xyz.terrariafabric.mixin;
+
+import bhw.voident.xyz.terrariafabric.currency.CoinCurrencySystem;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Inventory.class)
+public abstract class InventoryCoinAutoConvertMixin {
+
+    @Shadow
+    @Final
+    public Player player;
+
+    @Inject(method = "setChanged", at = @At("TAIL"))
+    private void terrariafabric$autoConvertCoinsOnInventoryChange(CallbackInfo ci) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            CoinCurrencySystem.tryAutoConvertNow(serverPlayer);
+        }
+    }
+}
+
